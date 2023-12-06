87d3925d36de0ef041d552d832774d6a9a5cf4cd
beyond-1234 <beyond_1234@live.com>
2023-12-07 01:03:10 +0800

    add ffmpeg wrapper and threaded command executor, progress monitoring is done

---
+++ app/common/converter/ffmpeg_executor.py
@@ -0,0 +1,64 @@
+import subprocess
+import threading
+
+from ..entity.task import Task
+from ..signal_bus import signalBus
+
+class FFmpegExecutor(threading.Thread):
+
+    def __init__(self, command, task: Task):
+        threading.Thread.__init__(self)
+        self.command = command
+        self.task = task
+
+        # pattern 1 is not been used
+        self.progressPattern1 = "size=\\s*([0-9]+)kB\\s+time=\\s*([0-9]+\\.[0-9]+)\\s+bitrate=\\s*([0-9]+\\.[0-9]+)kbits/s"
+        self.progressPattern2 = "size=\\s*([0-9]+)kB\\s+time=\\s*([0-9][0-9]):([0-9][0-9]):([0-9][0-9](\\.[0-9][0-9]?)?)\\s+"
+        self.durationPattern = "Duration:\\s+([0-9][0-9]):([0-9][0-9]):([0-9][0-9](\\.[0-9][0-9]?)?)"
+
+    def run(self):
+        duration = 0
+        progress = 0
+
+        process = subprocess.Popen(
+            self.command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True
+        )
+        while True:
+            output = process.stderr.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                o = output.strip()
+
+                # if video speed has changed, duration will change also
+                # need to take care of this
+                if duration == 0:
+                    durationMatch = re.search(self.durationPattern, o)
+                    if durationMatch:
+                        hour = int(durationMatch.group(1))
+                        min = int(durationMatch.group(2))
+                        sec = float(durationMatch.group(3))
+                        duration = hour*3600 + min*60 + sec;
+
+                if duration == 0:
+                    print("duration is 0 skipping")
+                    continue
+
+                progressMatch = re.search(self.progressPattern2, o)
+                if progressMatch:
+                    hour = int(progressMatch.group(2))
+                    min = int(progressMatch.group(3))
+                    sec = float(progressMatch.group(4))
+                    progress = hour*3600 + min*60 + sec;
+
+                signalBus.updateProgressSignal.emit(self.task.code, (progress / duration * 100))
+
+            if duration != 0 and progress > 99:
+                signalBus.updateProgressSignal.emit(self.task.code, 100)
+
+        process.stdout.close()
+        process.stderr.close()
---
+++ app/common/converter/ffmpeg_wrapper.py
@@ -0,0 +1,29 @@
+
+from ..entity.task import Task
+from .ffmpeg_executor import FFmpegExecutor
+
+class FFmpegWrapper:
+
+    def __init__(self, task: Task):
+        self.commandList = ['ffmpeg'];
+        self.task = task
+
+    def _addInputFile(self, filePath):
+        self.commandList.append("-i")
+        self.commandList.append(filePath)
+
+    def _addOutputFile(self, filePath):
+        self.commandList.append(filePath)
+
+    def _addCopyOption(self, isVideoCopy, isAudioCopy):
+        if isVideoCopy:
+            self.commandList.append("-c:v")
+            self.commandList.append("copy")
+        if isAudioCopy:
+            self.commandList.append("-c:a")
+            self.commandList.append("copy")
+
+    def startTask(self):
+        command = ' '.join(self.commandList)
+        thread = FFmpegExecutor(command, self.task)
+        thread.start()
